6d843f21a45ed02f1bf4a7000867c86ba1ec2f79
David Sasson <david.e.sasson@gmail.com>
2022-12-28 20:51:40 -0800

    added favicon

--- js/escape.js
+++ js/escape.js
@@ -2,6 +2,6 @@ window.onkeyup = function(e) {
     var event = e.which || e.keyCode || 0; // .which with fallback
 
     if (event == 27) { // ESC Key
-        window.location.href = '/'; // Navigate to URL
+        window.location.href = '/meditations'; // Navigate to URL
     }
 }
